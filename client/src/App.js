5385d99e786ccedcd1c5552e287b76c3f3550105
user.email <jobayer.ahmed109@gmail.com>
2023-07-30 13:11:38 +0600

    Login route created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { LoginPage } from "./routes/Routes";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
